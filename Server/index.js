f202bbddb458c4ec16e1d1f0dea3fc643406893e
James Curran <dzejms@gmail.com>
2020-08-02 06:29:35 -0400

    added yaml file

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
 app.get('/', (req, resp) => {
     resp.sendFile(__dirname + '/index.html');
